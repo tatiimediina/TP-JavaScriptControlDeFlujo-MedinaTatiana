b1080c9914c1a6e0213101ad7b54fbba7462b3f9
Tatiana Medina <tatymedina284@gmail.com>
2024-05-13 14:22:55 -0300

    Se realizó el ejercicio uno

---
+++ assets/js/num1al10.js
@@ -0,0 +1,11 @@
+let num = 0;
+
+
+while (num <= 10) {
+    if (num % 2 === 0){
+        console.log(`${num} es par`);
+    } else {
+        console.log(`${num} es impar`);
+    }
+    num = num + 1;
+}
