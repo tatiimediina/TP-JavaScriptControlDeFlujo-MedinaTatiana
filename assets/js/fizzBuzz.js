e0c510c027c2fbe8ad98e07678119de64221055a
Tatiana Medina <tatymedina284@gmail.com>
2024-05-13 15:54:50 -0300

    Trabajo finalizado

--- assets/js/fizzBuzz.js
+++ assets/js/fizzBuzz.js
@@ -1,4 +1,3 @@
-
 for (let i = 1; i <= 15; i++){
     if (i % 3=== 0 && i % 5 === 0){
         console.log(i + " FizzBuzz");
--- assets/js/num1al10.js
+++ assets/js/num1al10.js
@@ -1,11 +1,12 @@
 let num = 0;
 
-
 while (num <= 10) {
-    if (num % 2 === 0){
+    if (num === 0) {
+        console.log(num);
+    } else if (num % 2 === 0) {
         console.log(`${num} es par`);
     } else {
         console.log(`${num} es impar`);
     }
-    num = num + 1;
+    num++;
 }
